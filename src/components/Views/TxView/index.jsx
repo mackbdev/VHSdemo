9920f2c8a3377d39816f602f77a2610292ee926b
mackbdev <mackenziebakshdev@gmail.com>
2022-04-11 02:44:38 -0400

    css updates

--- src/components/Views/TxView/index.jsx
+++ src/components/Views/TxView/index.jsx
@@ -31,7 +31,7 @@ const TxView = ({ props, animations }) => {
                             <LoadingBar props={{ msg: 'This Block Has No TXs....Check Another!', showLoader: false }} /> :
                             loadingTxViewData ? <LoadingBar props={{ msg: 'Loading Tx Data....', showLoader: true }} /> : blockSelectedTxSendingEth.map((data) => {
                                 let childProps = { parentProps: props, data };
-                                return <TxsPill key={data.txHash} props={childProps} />
+                                return <TxsPill key={data.txHash} props={childProps} animations={animations}/>
                             })
                         }
                     </DivContainer>
